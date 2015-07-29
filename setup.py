5a04335824fdc1c6c085038a9d421c157093eead
Oliver Bestmann <oliver.bestmann@esailors.de>
2015-07-29 08:55:53 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="avrolight",
+      version="1.0.0",
+      description="A light and fast implementation of the avro message format",
+      author="Oliver Bestmann",
+      author_email="oliver.bestmann@googlemail.com",
+      url="https://github.com/oliverbestmann/avrolight",
+      packages=["avrolight"])
